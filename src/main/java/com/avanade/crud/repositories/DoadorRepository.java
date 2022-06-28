604082282db2d3ac9cf6de222ae27b52ac2320cd
geniinho <geniinho@hotmail.com>
2022-06-28 17:55:36 -0300

    correção de bug nos repositories

--- src/main/java/com/avanade/crud/repositories/DoadorRepository.java
+++ src/main/java/com/avanade/crud/repositories/DoadorRepository.java
@@ -4,6 +4,9 @@ import com.avanade.crud.domain.Doador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface DoadorRepository extends JpaRepository<Doador,Long> {
+public interface DoadorRepository extends JpaRepository<Doador,Integer> {
+    Optional<Doador> findByEmail(String email);
 }
--- src/main/java/com/avanade/crud/repositories/DonatarioRepository.java
+++ src/main/java/com/avanade/crud/repositories/DonatarioRepository.java
@@ -5,6 +5,9 @@ import com.avanade.crud.domain.Donatario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface DonatarioRepository extends JpaRepository<Donatario,Long> {
+public interface DonatarioRepository extends JpaRepository<Donatario,Integer> {
+    Optional<Donatario> findByEmail(String email);
 }
