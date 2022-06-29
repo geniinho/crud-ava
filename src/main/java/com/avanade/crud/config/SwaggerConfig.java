9b7c81a2289ac061254de704db94f99dfc7ff798
geniinho <geniinho@hotmail.com>
2022-06-29 12:03:45 -0300

    adicionando dados na pagina inicial do swagger

--- src/main/java/com/avanade/crud/config/SwaggerConfig.java
+++ src/main/java/com/avanade/crud/config/SwaggerConfig.java
@@ -1,13 +1,18 @@
 package com.avanade.crud.config;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
+
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 
 
 @Configuration
@@ -19,6 +24,24 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Doação API REST",
+                "API REST de cadastro de doações.",
+                "1.0.0",
+                "Terms of Service",
+                new Contact("Genivaldo Alves", "https://www.myinstants.com/pt/instant/som-inentendivel-rodrigo-faro-88868/",
+                        "email@email.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html",
+                new ArrayList<>()
+        );
+
+        return apiInfo;
     }
 }
